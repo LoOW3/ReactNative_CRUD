d1a16518d99bc92a486a9303e6b9cd3892013317
Ignacio Diaz <marketscenter.pg@gmail.com>
2022-06-01 03:07:35 -0300

    addScreen functionality

--- src/screens/Add.js
+++ src/screens/Add.js
@@ -1,10 +1,56 @@
-import { Text, View, StyleSheet } from 'react-native'
-import React from 'react'
+import { Text, View, StyleSheet, TextInput, Button } from 'react-native'
+import React, { useState } from 'react'
+import EmojiPicker from 'rn-emoji-keyboard'
+import { database } from '../config/fb'
+import { collection, addDoc } from 'firebase/firestore'
+import { useNavigation } from '@react-navigation/native'
 
 export default function Add() {
+    const navigation = useNavigation();
+    const [isOpen, setIsOpen] = useState(false)
+    const [newItem, setNewItem] = useState({
+        emoji: '😀',
+        name: '',
+        price: 0,
+        isSold: false,
+        createAt: new Date()
+    })
+
+    const onSend = async() =>{
+        await addDoc(collection(database, 'products'), newItem);
+        navigation.goBack();
+    }
+
+    const handlePick = (EmojiObject) => {
+        setNewItem({
+            ...newItem,
+            emoji: EmojiObject.emoji
+        })
+    };
+
   return (
     <View style={styles.container}>
-      <Text>Add</Text>
+      <Text style={styles.title}>Sell a new Product</Text>
+      <Text style={styles.emoji} onPress={() => setIsOpen(true)}>
+          {newItem.emoji}
+      </Text>
+      <EmojiPicker 
+        onEmojiSelected={handlePick}
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+      />
+      <TextInput 
+        onChangeText={(text) => setNewItem({...newItem, name: text})}
+        placeholder='Porduct name'
+        style={styles.inputContainer}
+      />
+      <TextInput 
+        onChangeText={(text) => setNewItem({...newItem, price: text})}
+        placeholder='$ Price'
+        style={styles.inputContainer}
+        keyboardType='number-pad'
+      />
+      <Button title='Publish' onPress={onSend} />
     </View>
   )
 }
@@ -14,6 +60,29 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center'
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: '700'
+    },
+    inputContainer: {
+        width: '90%',
+        padding: 13,
+        marginVertical: 6,
+        borderWidth: 1,
+        borderColor: '#ddd',
+        borderRadius: 6
+    },
+    emoji: {
+        fontSize: 100,
+        borderWidth: 1,
+        borderColor: '#ddd',
+        borderRadius: 6,
+        padding: 10,
+        marginVertical: 6,
+
     }
+
+
 })
 
