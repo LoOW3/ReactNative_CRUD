2e62d78e1e4be679c3e8118794bb5ccaeae82b28
Ignacio Diaz <marketscenter.pg@gmail.com>
2022-06-01 02:10:05 -0300

    config firebase - creacion components

---
+++ src/config/fb.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import  Constants  from "expo-constants";
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: Constants.manifest.extra.apiKey,
+    authDomain: Constants.manifest.extra.AuthDomain,
+    projectId: Constants.manifest.extra.projectId,
+    storageBucket: Constants.manifest.extra.storageBucket,
+    messagingSenderId: Constants.manifest.extra.messagingSenderId,
+    appId: Constants.manifest.extra.appId
+};
+
+initializeApp(firebaseConfig);
+
+export const database = getFirestore();
