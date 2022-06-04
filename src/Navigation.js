00ae94261f005b69c3f925c25365aee92dd82adf
Ignacio Diaz <marketscenter.pg@gmail.com>
2022-06-04 02:07:35 -0300

    cambio fontFamily navigation

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,5 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from 'expo-font';
+
 
 import Home from "./screens/Home.js";
 import Add from "./screens/Add.js";
@@ -9,13 +11,31 @@ const Stack = createNativeStackNavigator();
 
 
 function MyStack() {
+    const [loaded] = useFonts({
+        Dancing: require('../assets/fonts/DancingScript-SemiBold.ttf'),
+      });
+      if (!loaded) {
+        return null;
+      }
     return( 
         <Stack.Navigator>
-            <Stack.Screen name='Home' component={Home}/>
+            <Stack.Screen 
+                name='Products' 
+                component={Home}
+                options={
+                    {headerTitleStyle: {
+                    fontFamily: 'Dancing',
+                    fontSize: 35,
+                  }}}
+            />
             <Stack.Screen 
                 name='Add' 
                 component={Add}
-                options={{presentation: 'modal'}}
+                options={{presentation: 'modal',
+                headerTitleStyle: {
+                    fontFamily: 'Dancing',
+                    fontSize: 35,
+                  }}}
             />
         </Stack.Navigator>
     )
