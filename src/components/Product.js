7f9c06b2fcb297d9d7ee3dc41630181d768e77a5
Ignacio Diaz <marketscenter.pg@gmail.com>
2022-06-04 02:08:09 -0300

    poder agregar stock y un poco de css

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,39 +1,71 @@
-import React from 'react'
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import React, { useState} from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native'
 import { database } from '../config/fb'
 import { deleteDoc, doc, updateDoc } from 'firebase/firestore'
-import { AntDesign } from '@expo/vector-icons'
+import { AntDesign } from '@expo/vector-icons';
+import { useFonts } from 'expo-font';
 
-export default function Product({ id, emoji, name, price, isSold }) {
-
+export default function Product({ id, emoji, name, price, isSold, stock}) {
+    const [q, setQ] = useState(1)
+    const [total,setTotal] = useState('')
     const onEdit = () => {
         const docRef = doc(database, 'products', id);
         updateDoc(docRef, {
-            isSold: true,
+            stock: stock - q
         })
+        setQ(1)
     }
 
     const onDelete = () => {
         const docRef = doc(database, 'products', id);
         deleteDoc(docRef)
     }
+    const [loaded] = useFonts({
+        PoppinsBold: require('../../assets/fonts/Poppins-Bold.ttf'),
+        PoppinsRegular: require('../../assets/fonts/Poppins-Regular.ttf')
+      });
 
+    function sumQ(){
+        if(q < stock) setQ(q + 1)
+    }
+    function restQ(){
+        if(q > 1) setQ(q - 1)
+    }
   return (
     <View style={styles.productContainer}>
         <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
             <Text style={styles.emoji}>{emoji}</Text>
             <AntDesign onPress={onDelete} name="delete"  size={26}/>
         </View>
-        <Text style={styles.name}>{name}</Text>
-        <Text style={styles.price}>$ {price}</Text>
+        <View style={{flexDirection: 'row', alignItems: 'center',justifyContent: 'space-between'}}>
+            <View>
+                <Text style={styles.name}>{name}</Text>
+                <Text style={styles.price}>$ {price}</Text>
+                <Text style={styles.stock}>Stock: {stock? stock : 0}ud</Text>
+            </View>
+            <View style= {{alignItems: 'center'}}>
+                <View style={styles.containerQuatity}>
+                    <TouchableOpacity  title='-' style={styles.moreLess} onPress={restQ}>
+                        <Text style={styles.buttonText}>-</Text>
+                    </TouchableOpacity>
+                    <View style={{width: 10}}></View>
+                    <TouchableOpacity  title='+' style={styles.moreLess} onPress={sumQ}>
+                        <Text style={styles.buttonText} >+</Text>
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    <Text style={styles.total}>Total: ${price * q}</Text>
+                </View>
+            </View>
+        </View>
         {
-        isSold ? 
+        isSold || !stock ? 
             (<TouchableOpacity style={[styles.button, {backgroundColor: 'grey'}]}>
                 <Text style={styles.buttonText}>Sold</Text>
             </TouchableOpacity>) 
             : 
             (<TouchableOpacity onPress={onEdit} style={styles.button}>
-                <Text style={styles.buttonText}>Purchase</Text>
+                <Text style={styles.buttonText}>Sell {q === 1? '': `x${q}`}</Text>
             </TouchableOpacity>)
         }
         
@@ -59,8 +91,12 @@ const styles = StyleSheet.create({
     },
     price: {
         fontSize: 20,
-        fontWeight: 'bold',
         color: 'gray'
+    },
+    stock: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        color: 'grey'
     },
     button: {
         backgroundColor: '#0fa5e9',
@@ -74,5 +110,23 @@ const styles = StyleSheet.create({
         fontSize: 24,
         fontWeight: 'bold',
         color: '#fff'
+    },
+    containerQuatity: {
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        height: 40
+    },
+    moreLess: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 2,
+        paddingHorizontal: 20,
+        borderRadius: 4,
+        elevation: 3,
+        backgroundColor: '#0fa5e9',
+    },
+    total: {
+        marginTop: 5,
+        color: 'grey'
     }
 })
