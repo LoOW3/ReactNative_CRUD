a4f00e4606060a8c75a9371914467cfb969564d5
Ignacio Diaz <marketscenter.pg@gmail.com>
2022-06-08 21:42:02 -0300

    agregado bottom navigation

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import Navigation from './src/Navigation';
+import Navigation from './src/navigation/Navigation';
 import { store } from "./src/redux/store";
 import { Provider } from "react-redux";
 
